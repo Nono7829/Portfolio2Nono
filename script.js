d68b2118b1fb177f0b7ebddba5aef907629834cb
Nono7829 <143123296+Nono7829@users.noreply.github.com>
2026-02-03 15:18:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+let languadeCode = ["Python", "HTML", "CSS", "JavaScript", "PHP"];
+let languageCodeText = languadeCode[i];
 
+const delay = 3;
+const isRunning = true;
+
+while (isRunning == true){
+    document.getElementById("language-code-id").textContent = "Updated Text!";
+
+}
